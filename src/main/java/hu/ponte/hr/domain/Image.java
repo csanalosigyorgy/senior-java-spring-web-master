30164b2bf57670cb7885059373e2d32ac007c912
Csanálosi György <csanalosigyorgy@gmail.com>
2022-09-23 18:42:41 +0200

    create Image entity

---
+++ src/main/java/hu/ponte/hr/domain/Image.java
@@ -0,0 +1,28 @@
+package hu.ponte.hr.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String mimeType;
+
+    @Column(nullable = false)
+    private long size;
+
+}
